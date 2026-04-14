5db90d206bdf26b03a0e4c0444ae4351e90bc540
Aditya Singh <133494121+AdityaSingh-100@users.noreply.github.com>
2026-04-14 21:07:03 +0530

    changed baseURL

--- server/configs/openai.ts
+++ server/configs/openai.ts
@@ -1,8 +1,8 @@
 import OpenAI from "openai";
 
+// baseURL: "https://api.groq.com/openai/v1",
 const openai = new OpenAI({
-  // baseURL: "https://openrouter.ai/api/v1",
-  baseURL: "https://api.groq.com/openai/v1",
+  baseURL: "https://openrouter.ai/api/v1",
   apiKey: process.env.AI_API_KEY,
 });
 
